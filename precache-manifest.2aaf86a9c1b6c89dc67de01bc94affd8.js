21503bd0a8669838b3ff2cadfa697807830eecf5
Muhammad Bagus Zulmi <baguszulmi101198@gmail.com>
2020-02-22 07:11:53 +0700

    mantaf

---
+++ precache-manifest.2aaf86a9c1b6c89dc67de01bc94affd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1d626a60eaa9856cac5de2be867f0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2763290c7536ff61c2ac",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5555f11b401eb3847aac",
+    "url": "/static/js/2.6ee563b2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.6ee563b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2763290c7536ff61c2ac",
+    "url": "/static/js/main.3f4f37ab.chunk.js"
+  },
+  {
+    "revision": "3acc6ac7b0772e3e50da",
+    "url": "/static/js/runtime-main.9e64c07d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
